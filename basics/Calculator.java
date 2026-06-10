3b56d4d91bc6366bafa5f9f76cb7cebbe3fbc489
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-06-10 22:00:10 +0530

    Add Calculator class for basic arithmetic operations

---
+++ basics/Calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        if (b != 0)
+            System.out.println("Division: " + (a / b));
+    }
+}
