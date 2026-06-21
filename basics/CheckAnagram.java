d01cd21bf84e8cd6a626dc076ff522c9fa71f7b0
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-06-21 23:14:33 +0530

    Add CheckAnagram class to check for anagrams

---
+++ basics/CheckAnagram.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class CheckAnagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if (Arrays.equals(a, b))
+            System.out.println("Anagram");
+        else
+            System.out.println("Not Anagram");
+    }
+}
