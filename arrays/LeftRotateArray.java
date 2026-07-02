e264ef36b04c8fd8c0ae1211964bb64230edae62
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-07-02 21:54:22 +0530

    Add LeftRotateArray class for array left rotation

---
+++ arrays/LeftRotateArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class LeftRotateArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int first = arr[0];
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[n - 1] = first;
+        System.out.print("Array after left rotation: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
