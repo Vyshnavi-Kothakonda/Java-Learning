7608b81cc8f1432799bbd9687f058ffd4d360412
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-07-05 22:34:18 +0530

    Implement binary search in BinarySearch.java

---
+++ arrays/BinarySearch.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int low = 0, high = n - 1;
+        boolean found = false;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                System.out.println("Element found at index: " + mid);
+                found = true;
+                break;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (!found) {
+            System.out.println("Element not found");
+        }
+    }
+}
