9d42045b40d3bbe9df30bf65a88cac7c8659215a
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-07-16 20:07:12 +0530

    Add Student class and main method for display

---
+++ oops/ClassAndObject.java
@@ -0,0 +1,16 @@
+class Student {
+    String name;
+    int age;
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+public class ClassAndObject {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Vyshnavi";
+        s1.age = 19;
+        s1.display();
+    }
+}
