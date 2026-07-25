516f05e4a31f3c0f815f1952501fd3cb55347413
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-07-25 22:27:44 +0530

    Implement Animal interface with Dog class

---
+++ oops/InterfaceDemo.java
@@ -0,0 +1,15 @@
+interface Animal {
+    void sound();
+}
+class Dog implements Animal {
+    @Override
+    public void sound() {
+        System.out.println("Dog barks.");
+    }
+}
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Animal animal = new Dog();
+        animal.sound();
+    }
+}
