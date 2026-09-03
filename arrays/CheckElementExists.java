3d5e8476b8e5083761ead7e582f333483aac10fb
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-09-03 22:41:44 +0530

    Add CheckElementExists class to find an element

---
+++ arrays/CheckElementExists.java
@@ -0,0 +1,18 @@
+public class CheckElementExists {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int target = 30;
+        boolean found = false;
+        for (int number : numbers) {
+            if (number == target) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("Element Found");
+        } else {
+            System.out.println("Element Not Found");
+        }
+    }
+}
