851e61e80bd4eb9d425547d47a72e1a4f94b3095
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-09-03 22:40:58 +0530

    Add FindArrayLength class to print array length

---
+++ arrays/FindArrayLength.java
@@ -0,0 +1,6 @@
+public class FindArrayLength {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50, 60};
+        System.out.println("Array Length: " + numbers.length);
+    }
+}
